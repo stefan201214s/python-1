91bd19e1406e17e9378ff2bed8e1cf3017918d40
stefan201214s <balaceanustefan@icloud.com>
2026-01-31 18:10:44 +0100

    nombre premier

--- l1.2.py
+++ l1.2.py
@@ -20,3 +20,20 @@ def estPremier(num):
 
 nombre = int(input("choisis un nombre de 1-10"))
 estPremier(nombre)
+
+def right_triangle(a, b, c):
+    if(a**2 == b**2 + c**2) or (b**2 == a**2 + c**2) or (c**2 == b**2 + a**2):
+        print("C'est un triangle rectangle")
+    else:
+        print("Ce n'est pas un triangle rectangle")
+
+right_triangle(3, 4, 5)
+right_triangle(4, 5, 6)
+
+def func_numbers(q, r, p):
+    a = p + q**2 + r**2
+    print(a)
+
+func_numbers(p=3, q=2, r=1)
+
+print(q)
