f072e3bc06ace094407062f915c22fd831cdaa0a
stefan201214s <balaceanustefan@icloud.com>
2026-05-16 18:19:50 +0200

    l12

--- l12.py
+++ l12.py
@@ -1,38 +1,35 @@
+def openFile():
+    file = open("test.txt","r")
+    data = file.read()
+    print(data)
+    file.close()
+openFile()
+def writeReadFile():
+    file = open("test.txt","a+")
+    file.write("helle le frere de stef")
+    file.seek(0)
+    data = file.read()
+    print(data)
+    file.close()
+#openFile()
+#writeReadFile()
 
-def task1():
-    nums=[1,1,1,2,2,2,1,1,3,3,4,5,4,5,6,7,6,7]
-    new_nums=[]
-    for num in nums:
-        if num not in new_nums:
-                new_nums.append(num)
-        print(nums)
-        print(new_nums)
-task1()
-def task2():
-    nums=[]
-    for i in range(10):
-        num=int(input("Give me num : "))
-        nums.append(num)
-    pair=0
-    impair=0
-    positive=0
-    negative=0
-    zero=0
-    for nbr in nums:
-        if nbr > 0:
-            positive+=1
-        if nbr < 0:
-            negative += 1
-        if nbr == 0:
-            zero += 1
-        if i%2 == 0:
-            pair+= 1
-        if i%2==1 : 
-            impair+=1
-    
-    print("pair: ",pair)
-    print("impair:",impair)
-    print(" negative:",negative)
-    print("zero", zero)
-    print("positive", positive)
-task2()
+
+def readLines():
+    file = open("test.txt","r")
+    data1 = file.read()
+    file.seek(0)
+    data2 = file.readlines()
+    file.seek(0)
+    data3 = file.readlines()
+    print(data1)
+    print(data2)
+    print(data3)
+readLines()
+
+def Rline():
+    file = open("test.txt","r")
+    data = file.readlines()
+    for line in data:
+        print(line)
+Rline()
